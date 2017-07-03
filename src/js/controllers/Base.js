f4c860d55f554994e2693b556a3df73dc5ddbb5d
andrey3aytsev <andrey3aytsev@yandex.ru>
2017-07-04 01:13:16 +0300

    fonts fix

--- src/js/controllers/Base.js
+++ src/js/controllers/Base.js
@@ -8,6 +8,7 @@ weatherApp.controller('BaseController',
             	if (data.data.id === 519690) {
             		data.data.name = 'Saint petersburg';
             	};
+                data.data.main.temp = Math.ceil(data.data.main.temp);
             	$timeout(function() {
 	                $scope.result = data.data;
 	                $scope.result.icon = '/dist/images/icons/' + data.data.weather[0].icon + '.svg';
